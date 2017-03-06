a5d4601f42bf6254b7db402fb60e85edcda96e6e
hyukwon <hyukwon@ebay.com>
2017-03-06 17:27:10 +0900

    Syntax Analysis Test

---
+++ RoslynTest/SampleMicroserviceServer1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynTest
+{
+    public class SampleMicroserviceServer1
+    {
+        public void GetSample1()
+        {
+            string uri = "http://www.auction.co.kr";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+
+            request.Method = WebRequestMethods.Http.Get;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+            string result = reader.ReadToEnd();
+
+            Console.WriteLine(result);
+        }
+
+        public void GetSample2(string[] args)
+        {
+        }
+
+        public void GetSample3(int id, string name)
+        {
+        }
+    }
+}
---
+++ RoslynTest/SyntaxAnalysisTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
+
+namespace RoslynTest
+{
+    public class SyntaxAnalysisTest
+    {
+
+        public void RunTest()
+        {
+            string fullPathName = @"..\..\SampleMicroserviceServer1.cs";
+
+            String csfile = File.ReadAllText(fullPathName);
+
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(csfile);
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+
+            root.ChildNodes().Where(_ => _.GetType().Equals(typeof(NamespaceDeclarationSyntax))).ToList().ForEach(namespaceNode => {
+
+
+                Console.WriteLine(string.Format("Namespace: {0}", ((NamespaceDeclarationSyntax)namespaceNode).Name));
+
+                namespaceNode.ChildNodes().Where(_ => _.GetType().Equals(typeof(ClassDeclarationSyntax))).ToList().ForEach(classNode => {
+
+
+                    Console.WriteLine(string.Format("└Class: {0}", ((ClassDeclarationSyntax)classNode).Identifier.ValueText));
+
+                    classNode.ChildNodes().Where(_ => _.GetType().Equals(typeof(MethodDeclarationSyntax))).ToList().ForEach(methodNode => {
+
+                        Console.WriteLine(string.Format("  └Method: {0}", ((MethodDeclarationSyntax)methodNode).Identifier.ValueText));
+
+                        methodNode.ChildNodes().Where(_ => _.GetType().Equals(typeof(ParameterListSyntax))).ToList().ForEach(parameterListNodes => {
+
+                            Console.WriteLine(string.Format("    └ParameterList: {0}", parameterListNodes.GetText()));
+
+                        });
+                    });
+                });
+            });
+        }
+    }
+}
